ec2244ba700af99880ac847c55ea4a2f62904602
Tom Hnatovsky <hnatovsky@xswd.cz>
2017-03-08 05:11:13 +0100

    FileIconGenerator update

--- examples/prepareIcons.php
+++ examples/prepareIcons.php
@@ -1,12 +1,12 @@
 <?php
 	require_once '../vendor/autoload.php';
 
-	$helper = new \FileIconGenerator\Helper();
+	$helper = new \xomax\FileIconGenerator\Helper();
 	$directory = 'icons';
 
 	$fileSystem = new \Symfony\Component\Filesystem\Filesystem();
 	$fileSystem->mkdir($directory);
 
-	$builder = new \FileIconGenerator\Builder();
+	$builder = new \xomax\FileIconGenerator\Builder();
 
 	$helper->create32x32($builder->getExtensions(), $directory);
